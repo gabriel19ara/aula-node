32817a36711121b7006348b96bc4c420551ccb18
gabriel19ara <gabriel19ara@gmail.com>
2024-08-13 19:40:00 -0300

    utilizando modulo nativo os e instalando o nodemon

---
+++ script.js
@@ -0,0 +1,10 @@
+//console.log("rodando no node");
+
+const os = require("os");
+
+/* console.log("Arquitetura:", os.arch());
+console.log("Plataforma:", os.platform());
+console.log("Homedir:", os.homedir()); */
+
+
+
